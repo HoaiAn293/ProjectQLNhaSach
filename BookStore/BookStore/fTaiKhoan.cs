3c76c6e641e186390c09dbd9ab0472459d2ab957
HoaiAn293 <lenguyenhoaian293@gmail.com>
2024-10-02 19:34:45 +0700

    push code

---
+++ BookStore/BookStore/fTaiKhoan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookStore
+{
+    public partial class fTaiKhoan : Form
+    {
+        public fTaiKhoan()
+        {
+            InitializeComponent();
+            txtMatKhau.UseSystemPasswordChar = true;
+        }
+
+        private void chkHienMatKhau_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkHienMatKhau.CheckState == CheckState.Checked)
+            {
+                txtMatKhau.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtMatKhau.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
