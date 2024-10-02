1dd93ce9acd410df4dacb72df46bfa8f6abb2f95
HoaiAn293 <lenguyenhoaian293@gmail.com>
2024-10-02 22:04:07 +0700

    git push

---
+++ fHoaiAn/fHoaDon.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace fHoaiAn
+{
+    partial class fHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnChitiet = new System.Windows.Forms.Button();
+            this.cmbTenS = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.btnHT = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.77391F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(318, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(122, 26);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "HÓA ĐƠN";
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(42, 49);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 49;
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(735, 265);
+            this.dgv1.TabIndex = 26;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(42, 331);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(159, 40);
+            this.btnThem.TabIndex = 27;
+            this.btnThem.Text = "THÊM / SỬA HĐ";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(338, 331);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(129, 40);
+            this.btnXoa.TabIndex = 28;
+            this.btnXoa.Text = "XÓA HÓA ĐƠN";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnChitiet
+            // 
+            this.btnChitiet.Location = new System.Drawing.Point(620, 331);
+            this.btnChitiet.Name = "btnChitiet";
+            this.btnChitiet.Size = new System.Drawing.Size(157, 40);
+            this.btnChitiet.TabIndex = 29;
+            this.btnChitiet.Text = "CHI TIẾT HÓA ĐƠN";
+            this.btnChitiet.UseVisualStyleBackColor = true;
+            this.btnChitiet.Click += new System.EventHandler(this.btnChitiet_Click);
+            // 
+            // cmbTenS
+            // 
+            this.cmbTenS.FormattingEnabled = true;
+            this.cmbTenS.Location = new System.Drawing.Point(135, 656);
+            this.cmbTenS.Name = "cmbTenS";
+            this.cmbTenS.Size = new System.Drawing.Size(146, 24);
+            this.cmbTenS.TabIndex = 37;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.01739F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 658);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "TÊN SÁCH";
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(44, 459);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.RowHeadersWidth = 49;
+            this.dgv2.RowTemplate.Height = 24;
+            this.dgv2.Size = new System.Drawing.Size(733, 179);
+            this.dgv2.TabIndex = 35;
+            // 
+            // btnHT
+            // 
+            this.btnHT.Location = new System.Drawing.Point(611, 757);
+            this.btnHT.Name = "btnHT";
+            this.btnHT.Size = new System.Drawing.Size(199, 45);
+            this.btnHT.TabIndex = 34;
+            this.btnHT.Text = "HOÀN THÀNH";
+            this.btnHT.UseVisualStyleBackColor = true;
+            this.btnHT.Click += new System.EventHandler(this.btnHT_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(665, 644);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 37);
+            this.button5.TabIndex = 33;
+            this.button5.Text = "CHỌN";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.01739F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(361, 658);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 20);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "SỐ LƯỢNG";
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(460, 656);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(146, 22);
+            this.domainUpDown1.TabIndex = 31;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.77391F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(318, 420);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 26);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "CHỌN SÁCH";
+            // 
+            // fHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 814);
+            this.Controls.Add(this.cmbTenS);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.btnHT);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnChitiet);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.label10);
+            this.Name = "fHoaDon";
+            this.Text = "HÓA ĐƠN";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnChitiet;
+        private System.Windows.Forms.ComboBox cmbTenS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Button btnHT;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
