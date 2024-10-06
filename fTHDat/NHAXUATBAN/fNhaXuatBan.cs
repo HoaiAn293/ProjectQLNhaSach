afb0cff00f80d1b1bad1e0db1f167f6b6f5783a5
HoaiAn293 <lenguyenhoaian293@gmail.com>
2024-10-06 18:40:54 +0700

    pull code

---
+++ fTHDat/NHAXUATBAN/fNhaXuatBan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NHAXUATBAN
+{
+    public partial class fNhaXuatBan : Form
+    {
+        public fNhaXuatBan()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("MATG", typeof(string));
+
+            table.Rows.Add("DANH SÁCH CÁC NHÀ XUẤT BẢN HIỆN CÓ");
+
+            table.Rows.Add("NXB KIM DONG");
+
+            table.Rows.Add("NXB TUOI TRE");
+            table.Rows.Add("NXB GIAO DUC");
+
+
+            // Liên kết DataTable với DataGridView
+            dataGridView1.DataSource = table;
+            dataGridView1.ColumnHeadersVisible = false;
+        }
+    }
+    }
+
---
+++ fTHDat/NOP/Fsuatacgia.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace NOP
+{
+    partial class fSuaTacGia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtQUE = new System.Windows.Forms.TextBox();
+            this.txtTEN = new System.Windows.Forms.TextBox();
+            this.txtMA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(445, 349);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(768, 132);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 31;
+            this.checkBox1.Text = "CHƯA RÕ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // txtQUE
+            // 
+            this.txtQUE.Location = new System.Drawing.Point(548, 132);
+            this.txtQUE.Name = "txtQUE";
+            this.txtQUE.Size = new System.Drawing.Size(200, 20);
+            this.txtQUE.TabIndex = 30;
+            // 
+            // txtTEN
+            // 
+            this.txtTEN.Location = new System.Drawing.Point(548, 82);
+            this.txtTEN.Name = "txtTEN";
+            this.txtTEN.Size = new System.Drawing.Size(249, 20);
+            this.txtTEN.TabIndex = 29;
+            // 
+            // txtMA
+            // 
+            this.txtMA.Location = new System.Drawing.Point(548, 25);
+            this.txtMA.Name = "txtMA";
+            this.txtMA.Size = new System.Drawing.Size(249, 20);
+            this.txtMA.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(463, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "TÊN TÁC GIẢ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(467, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "QUÊ QUÁN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(463, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "MÃ TÁC GIẢ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 50);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Chọn";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(679, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 50);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Lưu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // fSuaTacGia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtQUE);
+            this.Controls.Add(this.txtTEN);
+            this.Controls.Add(this.txtMA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "fSuaTacGia";
+            this.Text = "SuaTacGia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtQUE;
+        private System.Windows.Forms.TextBox txtTEN;
+        private System.Windows.Forms.TextBox txtMA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
